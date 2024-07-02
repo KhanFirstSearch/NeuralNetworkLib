a4d0186e0f5744d871866205b3a03d268bdeb38f
KhanFirstSearch <yousufskhan2005@gmail.com>
2024-07-01 22:37:56 -0500

    added a tanh method for different neural networks types

--- Math.py
+++ Math.py
@@ -1,16 +1,21 @@
 import numpy as np
 
 def sigmoid(x, deriv=False):
-    if (deriv):
+    if deriv:
         return x * (1 - x)
     return 1 / (1 + np.exp(-x))
 
 def relu(x, deriv=False):
-    if (deriv):
+    if deriv:
         return np.where(x > 0, 1, 0)
     return np.maximum(0, x)
 
+def tanh(x, deriv=False):
+    if deriv:
+        return 1 - np.tanh(x) ** 2
+    return np.tanh(x)
+
 def mse_loss(y_true, y_pred, deriv=False):
-    if (deriv):
+    if deriv:
         return 2 * (y_pred - y_true) / y_true.size
     return np.mean((y_true - y_pred) ** 2)
